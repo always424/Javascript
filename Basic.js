3fa1790a80d568e4fb0c748bbfc7d34ed06c7c8b
Nafis Abdullah <nafisabdullah424@gmail.com>
2025-03-10 23:07:40 +0600

    Create Basic.js

---
+++ Basic.js
@@ -0,0 +1,27 @@
+// To print any thing
+
+console.log("Hello World");
+console.log("I like pizza.");
+
+window.alert(`This is an alert`);
+
+window.alert(`I like pizza.`);
+
+document.getElementById("Hello").textContent = `Hello`;
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <link rel="stylesheet" href="/Website/style.css">
+// </head>
+// <body>
+//     <h1 id="Hello"></h1>
+
+//     <script src="/Website/index.js"></script>
+// </body>
+// </html>
