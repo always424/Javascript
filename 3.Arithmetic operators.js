db797dd851c2cfb011c89cf974b8e15512e5f8e1
Nafis Abdullah <nafisabdullah424@gmail.com>
2025-03-10 23:36:57 +0600

    Create 3.Arithmetic operators.js

---
+++ 3.Arithmetic operators.js
@@ -0,0 +1,22 @@
+// arithmetic operators
+
+let students = 21;
+
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+students = students ** 2;
+students = students % 2;
+
+console.log(students);
+
+// Operator Precedence
+// 1. Parentheses
+// 2. Exponents
+// 3. Multiplication and Division (from left to right)
+// 4. Addition and Subtraction (from left to right)
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
