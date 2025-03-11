69081c918def194e472ceb94c1923c8e1e8d322d
Nafis Abdullah <nafisabdullah424@gmail.com>
2025-03-12 01:13:46 +0600

    Update 6.Const.js

--- 6.Const.js
+++ 6.Const.js
@@ -1,17 +1,38 @@
 // const
 
 
-const pi = 3.14159;
+const PI = 3.14159;
 
 let radius;
-
 let circumference;
 
+document.getElementById("button").onclick = function(){
+    radius=document.getElementById("text").value;
 
-radius = window.prompt("Enter the radius of a circle");
+    radius = Number(radius);
 
-radius = Number(radius);
+    circumference = 2 * PI * radius;
 
-circumference = 2 * pi * radius;
+    document.getElementById("h").textContent = circumference;
 
-console.log(circumference);
+
+}
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href"/style.css">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>Enter The Radius of a Circle:</h1>
+//     <label for="">Radius:</label>
+//     <input type="text" id="text">
+//     <button id="button">Submit</button>
+//     <h3 id="h"></h3>
+//     <script src="/script.js"></script>
+// </body>
+// </html>
