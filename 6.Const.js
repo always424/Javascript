2d02d79c4e91a04091b6947053f65de64a731c65
Nafis Abdullah <nafisabdullah424@gmail.com>
2025-03-12 01:00:02 +0600

    Create 6.Const.js

---
+++ 6.Const.js
@@ -0,0 +1,17 @@
+// const
+
+
+const pi = 3.14159;
+
+let radius;
+
+let circumference;
+
+
+radius = window.prompt("Enter the radius of a circle");
+
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+
+console.log(circumference);
