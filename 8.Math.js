23faf384bb59f0b0060253f0223c787bb57e0a3e
Nafis Abdullah <nafisabdullah424@gmail.com>
2025-03-12 17:50:35 +0600

    Create 8.Math.js

---
+++ 8.Math.js
@@ -0,0 +1,20 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+
+let x = 3.5;
+let y = 2;
+let z;
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(x);
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.cin(x);
+z = Math.tan(x);
+z = Math.cos(x);
+
+
+console.log(z);
